59cfdf2bf24b82c1d84958ea4884fe5cffdbf253
‘CptKay’ <sammerz@bluewin.ch>
2023-01-12 16:18:50 +0100

    000 init

---
+++ includes/header.php
@@ -0,0 +1,17 @@
+<header>
+<nav>
+		<ol class="cd-multi-steps text-center custom-icons">
+			<li class="visited"><a href="#0">Question 01</a></li>
+			<li class="visited" ><a href="#0">Question 02</a></li>
+            <li class="visited" ><a href="#0">Question 03</a></li>
+            <li class="visited" ><a href="#0">Question 04</a></li>
+            <li class="visited" ><a href="#0">Question 05</a></li>
+            <li class="visited" ><a href="#0">Question 06</a></li>
+            <li class="visited" ><a href="#0">Question 07</a></li>
+            <li class="visited" ><a href="#0">Question 08</a></li>
+            <li class="visited" ><a href="#0">Question 09</a></li>
+			<li class="current"><em>Question 10</em></li>
+			<li><em>Evaluation</em></li>
+		</ol>
+	</nav>
+</header>
