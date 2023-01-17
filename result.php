52f5a10045b19c85474de8692e08eded4dad33dd
‘CptKay’ <sammerz@bluewin.ch>
2023-01-17 15:04:59 +0100

    q07-q10

--- result.php
+++ result.php
@@ -1,7 +1,7 @@
     <!-- HEADER  -->
     <?php
     
-    include "./includes/tools.php";
+    // include "./includes/tools.php";
     include "./includes/header.php";
        ?>
     <!-- END:HEADER -->
@@ -10,13 +10,13 @@
         // Data question 01;
 
 
-        if(isset($_POST["RangeSlider-q01"])){
-            echo "Physical health:".$_POST["RangeSlider-q01"];
+        /* if(isset($_POST["RangeSlider-q01"])){
+            echo "Physical health:".$_POST["RangeSlider-q01"]; */
 
             // Your Slider value is here.
-        } else{
+        /* } else{
         Echo "Please slide the Slider Bar and Press Submit.";
-        }
+        } */
 
         ?>
     
